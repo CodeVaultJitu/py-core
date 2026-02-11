3b36bac823a4f44841539b92888cb739b74d2cb1
j1tu <jituballavhazarika@gmail.com>
2026-02-11 12:22:20 +0530

    heds or tails game using Python random module

--- lists_and_randomness/random_module.py
+++ lists_and_randomness/random_module.py
@@ -1,12 +1,17 @@
 import random
 
 #random integer
-rand_int = random.randint(1, 10)
+rand_int = random.randint(1, 2)
 print(rand_int)
 
 #random float
-random_float_0to1 = random.random()
-print(random_float_0to1)
+# random_float_0to1 = random.random()
+# print(random_float_0to1)
+#
+# random_float = random.uniform(10, 12)
+# print(random_float)
 
-random_float = random.uniform(10, 12)
-print(random_float)
+if rand_int == 1:
+    print("You've got heads.")
+else:
+    print("You've got tails.")
