5ce53b38fa4b4ee8f4b3708802cfd88dfc9876ff
j1tu <jituballavhazarika@gmail.com>
2026-02-11 12:10:08 +0530

    Implement randomization logic using Python random module

---
+++ lists_and_randomness/random_module.py
@@ -0,0 +1,12 @@
+import random
+
+#random integer
+rand_int = random.randint(1, 10)
+print(rand_int)
+
+#random float
+random_float_0to1 = random.random()
+print(random_float_0to1)
+
+random_float = random.uniform(10, 12)
+print(random_float)
