3f529a249bde731de4ef1ec7ff909c0fb21f0cfa
j1tu <jituballavhazarika@gmail.com>
2026-02-18 10:39:41 +0530

    Implement loop-based condition evaluation and control flow

--- functions_and_while_loops/reeborg_world.py
+++ functions_and_while_loops/reeborg_world.py
@@ -25,7 +25,7 @@ move()
 turn_right()
 move()
 
-# excercise 3 , hurdle 3
+# excercise 3 , hurdle 1
 def turn_right():
     turn_left()
     turn_left()
@@ -44,3 +44,35 @@ def jump():
 for i in range(1, 7): #bcz 6 hurdles
 # for i in range(6): 0 to 6
     jump()
+
+#excercise 4, hurdle 2
+#use while loop
+while at_goal() != True:
+#while not at_goal():
+    jump()
+
+#excercise 4, hurdle 3
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def wall():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    while wall_in_front() == True:
+        wall()
+    if front_is_clear() == True and not at_goal():
+        move()
+# while not at_goal():
+#     if wall_in_front():
+#         wall()
+#     else:
+#         move()
